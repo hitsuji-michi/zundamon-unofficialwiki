6293c449dffe8ec1212744fbf9419f467a3f204a
hitsuji-michi <>
2022-02-05 01:36:18 +0900

    年表を追加

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,8 @@
 function footer(){
     var html = "";
-    html += '<ul>';
+    html += '<ul class="nonedot">';
     html += '<li><a href="./index.html">HOME</a></li>';
+    html += '<li><a href="./chronology.html">年表</a></li>';
     html += '<li><a href="./character-reference.html">設定一覧</a></li>';
     html += '<li><a href="./quote-inove.html">Web小説（アイノべ）引用セリフ</a></li>';
     html += '<li><a href="./voice-list.html">追加ボイスfile名一覧</a></li>';
