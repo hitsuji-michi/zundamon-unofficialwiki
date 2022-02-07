5e56ec2da8ef7f3de8bd06f88cc33f12910f0d87
hitsuji-michi <>
2022-02-07 16:45:05 +0900

    誤字修正-URL調整（試行）

--- js/footer.js
+++ js/footer.js
@@ -1,7 +1,7 @@
 function footer(){
     var html = "";
     html += '<ul class="nonedot">';
-    html += '<li><a href="/">HOME</a></li>';
+    html += '<li><a href="./">HOME</a></li>';
     html += '<li><a href="./chronology/">年表</a></li>';
     html += '<li><a href="./character-reference/">設定一覧</a></li>';
     html += '<li><a href="./quote-inove/">Web小説（アイノべ）引用セリフ</a></li>';
