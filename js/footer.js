4afefa7e9749570813e75ece189ad8e4d604aa13
hitsuji-michi <>
2022-02-07 16:35:21 +0900

    URL調整（試行）

--- js/footer.js
+++ js/footer.js
@@ -1,12 +1,12 @@
 function footer(){
     var html = "";
     html += '<ul class="nonedot">';
-    html += '<li><a href="./index.html">HOME</a></li>';
-    html += '<li><a href="./chronology.html">年表</a></li>';
-    html += '<li><a href="./character-reference.html">設定一覧</a></li>';
-    html += '<li><a href="./quote-inove.html">Web小説（アイノべ）引用セリフ</a></li>';
-    html += '<li><a href="./voice-list.html">追加ボイスfile名一覧</a></li>';
-    html += '<li><a href="./link.html">外部リンク一覧</a></li>';
+    html += '<li><a href="/">HOME</a></li>';
+    html += '<li><a href="./chronology/">年表</a></li>';
+    html += '<li><a href="./character-reference/">設定一覧</a></li>';
+    html += '<li><a href="./quote-inove/">Web小説（アイノべ）引用セリフ</a></li>';
+    html += '<li><a href="./voice-list/">追加ボイスfile名一覧</a></li>';
+    html += '<li><a href="./link/">外部リンク一覧</a></li>';
     html += '</ul>';
     document.write(html);
 }
