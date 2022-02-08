7bee4ec9762ccd965995ffefde2f80e862e1d7b7
hitsuji-michi <>
2022-02-09 04:29:46 +0900

    jsテスト

--- js/footer.js
+++ js/footer.js
@@ -1,12 +1,24 @@
-function footer(){
+function footer() {
+    var getUrl = location.href;
+    var baseUrl = window.location.origin;
+
     var html = "";
     html += '<ul class="nonedot">';
-    html += '<a href="./"><li>HOME</li></a>';
-    html += '<a href="./chronology/"><li>年表</li></a>';
+
+    if (getUrl != baseUrl) {
+        html += '<a href="./"><li>HOME</li></a>';
+    }
+
+    var value = baseUrl + "./chronology/";
+    if (getUrl != value) {
+        html += '<a href="./chronology/"><li>年表</li></a>';
+    }
+
     html += '<a href="./character-reference/"><li>設定一覧</li></a>';
     html += '<a href="./quote-inove/"><li>Web小説引用セリフ</li></a>';
     html += '<a href="./voice-list/"><li>追加ボイスfile名一覧</li></a>';
     html += '<a href="./link/"><li>外部リンク一覧</li></a>';
     html += '</ul>';
+
     document.write(html);
 }
