d936334592960bae409fa4fde6a06690e1f2ee88
hitsuji-michi <>
2022-02-09 09:16:08 +0900

    jsテスト#7

--- js/footer.js
+++ js/footer.js
@@ -5,7 +5,7 @@ function footer() {
     var html = "";
     html += '<ul class="nonedot">';
 
-    var value = baseUrl + "/zundamon-unofficialwiki/";
+    var value = baseUrl + "/zundamon-unofficialwiki/";;
     if (getUrl != value) {
         html += '<a href="../"><li>HOME</li></a>';
     }
@@ -13,36 +13,46 @@ function footer() {
     var value = baseUrl + "/zundamon-unofficialwiki/chronology/";
     if (getUrl != value) {
         html += '<a href="';
-        html += baseUrl;
-        html += '/zundamon-unofficialwiki/chronology/"><li>年表</li></a>';
+        html += value;
+        html += '"><li>年表</li></a>';
+    } else {
+        html += '<a href="#main"><li>ページ上部</li></a>';
     }
 
     var value = baseUrl + "/zundamon-unofficialwiki/character-reference/";
     if (getUrl != value) {
         html += '<a href="';
-        html += baseUrl;
-        html += '/zundamon-unofficialwiki/character-reference/"><li>設定一覧</li></a>';
+        html += value;
+        html += '"><li>設定一覧</li></a>';
+    } else {
+        html += '<a href="#main"><li>ページ上部</li></a>';
     }
 
     var value = baseUrl + "/zundamon-unofficialwiki/quote-inove/";
     if (getUrl != value) {
         html += '<a href="';
-        html += baseUrl;
-        html += '/zundamon-unofficialwiki/quote-inove/"><li>Web小説引用セリフ</li></a>';
+        html += value;
+        html += '"><li>Web小説引用セリフ</li></a>';
+    } else {
+        html += '<a href="#main"><li>ページ上部</li></a>';
     }
 
     var value = baseUrl + "/zundamon-unofficialwiki/voice-list/";
     if (getUrl != value) {
         html += '<a href="';
-        html += baseUrl;
-        html += '/zundamon-unofficialwiki/voice-list/"><li>追加ボイスfile名一覧</li></a>';
+        html += value;
+        html += '"><li>追加ボイスfile名一覧</li></a>';
+    } else {
+        html += '<a href="#main"><li>ページ上部</li></a>';
     }
 
-    var value = baseUrl + "/zundamon-unofficialwiki/voice-list/";
+    var value = baseUrl + "/zundamon-unofficialwiki/link/";
     if (getUrl != value) {
         html += '<a href="';
-        html += baseUrl;
-        html += '/zundamon-unofficialwiki/link/"><li>外部リンク一覧</li></a>';
+        html += value;
+        html += '"><li>外部リンク一覧</li></a>';
+    } else {
+        html += '<a href="#main"><li>ページ上部</li></a>';
     }
 
     html += '</ul>';
