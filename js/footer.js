226f1041f8aac0fb3f13151e69a3555bc169f408
hitsuji-michi <>
2022-02-09 09:00:48 +0900

    jsテスト#5

--- js/footer.js
+++ js/footer.js
@@ -12,13 +12,39 @@ function footer() {
 
     var value = baseUrl + "/zundamon-unofficialwiki/chronology/";
     if (getUrl != value) {
-        html += '<a href="../chronology/"><li>年表</li></a>';
+        html += '<a href="';
+        html += baseUrl;
+        html += '/chronology/"><li>年表</li></a>';
     }
 
-    html += '<a href="./character-reference/"><li>設定一覧</li></a>';
-    html += '<a href="./quote-inove/"><li>Web小説引用セリフ</li></a>';
-    html += '<a href="./voice-list/"><li>追加ボイスfile名一覧</li></a>';
-    html += '<a href="./link/"><li>外部リンク一覧</li></a>';
+    var value = baseUrl + "/zundamon-unofficialwiki/character-reference/";
+    if (getUrl != value) {
+        html += '<a href="';
+        html += baseUrl;
+        html += '/character-reference/"><li>設定一覧</li></a>';
+    }
+
+    var value = baseUrl + "/zundamon-unofficialwiki/quote-inove/";
+    if (getUrl != value) {
+        html += '<a href="';
+        html += baseUrl;
+        html += './quote-inove/"><li>Web小説引用セリフ</li></a>';
+    }
+
+    var value = baseUrl + "/zundamon-unofficialwiki/voice-list/";
+    if (getUrl != value) {
+        html += '<a href="';
+        html += baseUrl;
+        html += '/voice-list/"><li>追加ボイスfile名一覧</li></a>';
+    }
+
+    var value = baseUrl + "/zundamon-unofficialwiki/voice-list/";
+    if (getUrl != value) {
+        html += '<a href="';
+        html += baseUrl;
+        html += '/link/"><li>外部リンク一覧</li></a>';
+    }
+
     html += '</ul>';
 
     document.write(html);
